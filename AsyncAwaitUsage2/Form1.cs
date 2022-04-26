8895ab4bcfdaff970feb6f5b4ce15af6bddb4934
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-04-26 11:42:28 +0530

    first commit

---
+++ AsyncAwaitUsage2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace AsyncAwaitUsage2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int CountCharacters()
+        {
+            int count = 0;
+            using(StreamReader reader=new StreamReader(@"C:\Users\2091199\source\repos\AsyncAwaitUsage2\AsyncAwaitUsage2\Data.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
+        }
+        private async void btnProcessFile_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountCharacters);
+            task.Start();
+
+            lblCount.Text = "Processing File....Please Wait";
+            int count = await task;
+            lblCount.Text = count.ToString() +" characters in file";
+
+        }
+    }
+}
